f08125a55f28479c295cf4b4131581cbc7bcfa70
Andrew Wigginton <andrewwigginton@Andrews-MacBook-Pro.local>
2019-03-04 11:50:41 -0700

    build change

---
+++ build/precache-manifest.bcfac8a12c2a1cff3be5a33c53e93743.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d9f9607f6d943e7bd9471cecf0735c0",
+    "url": "/ninaandroo/static/media/ytbw.6d9f9607.svg"
+  },
+  {
+    "revision": "35c68035c2502c1ffc2947f220420deb",
+    "url": "/ninaandroo/static/media/mountains.35c68035.jpg"
+  },
+  {
+    "revision": "76a6d6b469c86982621d16f9703db59e",
+    "url": "/ninaandroo/static/media/mailbw.76a6d6b4.svg"
+  },
+  {
+    "revision": "4c226c04160e99e014dc7f5944492c30",
+    "url": "/ninaandroo/static/media/instabw.4c226c04.svg"
+  },
+  {
+    "revision": "8c5cc78771599631a656e9a06eff8d16",
+    "url": "/ninaandroo/static/media/Photo.8c5cc787.jpg"
+  },
+  {
+    "revision": "e7e3e28349d3a313fffa",
+    "url": "/ninaandroo/static/js/runtime~main.e7e3e283.js"
+  },
+  {
+    "revision": "91227d5903339831ad82",
+    "url": "/ninaandroo/static/js/main.91227d59.chunk.js"
+  },
+  {
+    "revision": "6d2579e784921e734d6a",
+    "url": "/ninaandroo/static/js/1.6d2579e7.chunk.js"
+  },
+  {
+    "revision": "91227d5903339831ad82",
+    "url": "/ninaandroo/static/css/main.59d9d833.chunk.css"
+  },
+  {
+    "revision": "fa6a5032bedf3b4c9e3de72d7c39dcca",
+    "url": "/ninaandroo/index.html"
+  }
+];
