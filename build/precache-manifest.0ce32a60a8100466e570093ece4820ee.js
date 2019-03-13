78cdbdb9ed35756e513d8a1f9982c6bd1eb1e161
Andrew Wigginton <andrewwigginton@Andrews-MacBook-Pro.local>
2019-03-13 12:10:48 -0600

    ammended build

---
+++ build/precache-manifest.0ce32a60a8100466e570093ece4820ee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d9f9607f6d943e7bd9471cecf0735c0",
+    "url": "/static/media/ytbw.6d9f9607.svg"
+  },
+  {
+    "revision": "67ab4fd1e346d495b79e7d2bc3d23e6d",
+    "url": "/static/media/mountains.67ab4fd1.jpg"
+  },
+  {
+    "revision": "76a6d6b469c86982621d16f9703db59e",
+    "url": "/static/media/mailbw.76a6d6b4.svg"
+  },
+  {
+    "revision": "4c226c04160e99e014dc7f5944492c30",
+    "url": "/static/media/instabw.4c226c04.svg"
+  },
+  {
+    "revision": "a74ddac03b3990aec2b86c18c17b6a9b",
+    "url": "/static/media/Photo.a74ddac0.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "61b4282400aefab6317a",
+    "url": "/static/js/main.61b42824.chunk.js"
+  },
+  {
+    "revision": "c9f1443667e754a57b07",
+    "url": "/static/js/1.c9f14436.chunk.js"
+  },
+  {
+    "revision": "61b4282400aefab6317a",
+    "url": "/static/css/main.2f3ef11b.chunk.css"
+  },
+  {
+    "revision": "ef9c9276a18185e8a81bbbc8efab911c",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ninaandroo/precache-manifest.87dd7698822deb7dfaaf88bd51090f15.js"
+  "/precache-manifest.0ce32a60a8100466e570093ece4820ee.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ninaandroo/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
